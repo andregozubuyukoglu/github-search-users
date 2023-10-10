e1e9d40c874af6d6c346b9c6da92bccae4a36dd3
Andre <andregozubuyuk@gmail.com>
2023-10-10 22:22:28 +0300

    Updated files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import { GithubProvider } from "./context/context"
 import { Auth0Provider } from "@auth0/auth0-react"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
@@ -14,7 +15,4 @@ root.render(
   </React.StrictMode>
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
