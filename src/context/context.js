71fa18560c38c77033b11bf7a753b230b76a13f5
Andre <andregozubuyuk@gmail.com>
2023-10-13 22:53:15 +0300

    Updated files

--- src/context/context.js
+++ src/context/context.js
@@ -7,12 +7,40 @@ import axios from "axios"
 const rootUrl = "https://api.github.com"
 
 const GithubContext = React.createContext()
+
 const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser)
   const [repos, setRepos] = useState(mockRepos)
   const [followers, setFollowers] = useState(mockFollowers)
+
+  const [requests, setRequests] = useState(0)
+  const [loading, setIsLoading] = useState(false)
+
+  const [error, setError] = useState({ show: false, msg: "" })
+
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data
+        setRequests(remaining)
+        if (remaining === 0) {
+          toggleError(true, "sorry you have exceeded")
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg })
+  }
+
+  useEffect(checkRequest, [])
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   )
