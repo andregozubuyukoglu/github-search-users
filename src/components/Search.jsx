2ce38c9d278debea3ae1fcfc976bf8e4f9c60e3b
Andre <andregozubuyuk@gmail.com>
2023-10-14 20:28:21 +0300

    Updated files

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,11 +5,12 @@ import { GithubContext } from "../context/context"
 
 const Search = () => {
   const [user, setUser] = React.useState("")
-  const { requests, error } = React.useContext(GithubContext)
+  const { requests, error, searchGithubUser } = React.useContext(GithubContext)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (user) {
+      searchGithubUser(user)
       // setUser()
     }
   }
--- src/context/context.js
+++ src/context/context.js
@@ -18,6 +18,19 @@ const GithubProvider = ({ children }) => {
 
   const [error, setError] = useState({ show: false, msg: "" })
 
+  const searchGithubUser = async (user) => {
+    toggleError()
+
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    )
+    if (response) {
+      setGithubUser(response.data)
+    } else {
+      toggleError(true, "there is no user with that username")
+    }
+  }
+
   const checkRequest = () => {
     axios(`${rootUrl}/rate_limit`)
       .then(({ data }) => {
@@ -39,7 +52,14 @@ const GithubProvider = ({ children }) => {
   useEffect(checkRequest, [])
   return (
     <GithubContext.Provider
-      value={{ githubUser, repos, followers, requests, error }}
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+      }}
     >
       {children}
     </GithubContext.Provider>
